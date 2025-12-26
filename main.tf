fb8362229c0af6095c25a7ed7b92cd084c596883
jaswanth kumar <jaswanthkumarjash2000@gmail.com>
2025-12-26 13:00:23 +0530

    Terraform Practice

--- main.tf
+++ main.tf
@@ -2,13 +2,14 @@ module "vpc" {
     source = "../Terraform-aws-vpc"
     cidr_block = var.cidr_block
     vpc_tags = var.vpc_tags
-    subnet_cidr = var.subnet_cidr
+    public_subnet_cidr = var.public_subnet_cidr
+    private_subnet_cidr = var.private_subnet_cidr
 }
 
-output "vpc_id" {
-    value = module.vpc.vpc_id
-}
+# output "vpc_id" {
+#     value = module.vpc.vpc_id
+# }
 
-output "azs" {
-    value = module.vpc.azs
-}
+# output "azs" {
+#     value = module.vpc.azs
+# }
--- variables.tf
+++ variables.tf
@@ -14,6 +14,10 @@ variable "igw_tags" {
     }
 }
 
-variable "subnet_cidr" {
+variable "public_subnet_cidr" {
     default = ["10.0.1.0/24", "10.0.2.0/24"]
+}
+
+variable "private_subnet_cidr" {
+    default = ["10.0.11.0/24", "10.0.12.0/24"]
 }
