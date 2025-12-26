626cae3f9a2257e8e0db0ed1c8e5301f54fcb4bd
jaswanth kumar <jaswanthkumarjash2000@gmail.com>
2025-12-26 13:05:01 +0530

    Terraform Practice

--- main.tf
+++ main.tf
@@ -4,6 +4,7 @@ module "vpc" {
     vpc_tags = var.vpc_tags
     public_subnet_cidr = var.public_subnet_cidr
     private_subnet_cidr = var.private_subnet_cidr
+    database_subnet_cidr = var.database_subnet_cidr
 }
 
 # output "vpc_id" {
--- variables.tf
+++ variables.tf
@@ -21,3 +21,7 @@ variable "public_subnet_cidr" {
 variable "private_subnet_cidr" {
     default = ["10.0.11.0/24", "10.0.12.0/24"]
 }
+
+variable "database_subnet_cidr" {
+    default = ["10.0.21.0/24", "10.0.22.0/24"]
+}
